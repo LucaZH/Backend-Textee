3f08f440b2b1013738f6ea212ae018647fb647ed
LucaZH <lucarandria1@gmail.com>
2023-03-02 00:40:44 +0300

    [fix]

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import re
 
 def get_unique_words(filename):
-    with open(filename, 'r') as file:
-        content = file.read()
-        pattern = r'\b\w+\b'
-        matches = re.findall(pattern, content)
-        keywords = set(matches)
-        return list(keywords)
-print(get_unique_words("fichier.txt"))
+    content = file.read()
+    pattern = r'\b\w+\b'
+    matches = re.findall(pattern, content)
+    keywords = set(matches)
+    return list(keywords)
+# print(get_unique_words("fichier.txt"))
+file="fichier.txt"
+def get_file_extension(file):
+    return file.split(".")[-1]
+def verify_extension(file_name):
+    if get_file_extension()=="txt":
+            t_txt = open(file_name, "r")
+            get_unique_words(t)
+        
+
